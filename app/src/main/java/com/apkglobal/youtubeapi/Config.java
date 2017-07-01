9ef2bf8b50971ad562409de1961ba1fb786df4c5
Saurabh <saurabh1065@gmail.com>
2017-07-02 00:52:14 +0530

    App working perfectly

---
+++ app/src/main/java/com/apkglobal/youtubeapi/Config.java
@@ -0,0 +1,10 @@
+package com.apkglobal.youtubeapi;
+
+/**
+ * Created by saura on 7/1/2017.
+ */
+public class Config {
+
+    public static final String DEVELOPER_KEY = "AIzaSyBQsOVn0PLra5vACJByxiLEe3t6RSq7Xl4";
+    public static final String YOUTUBE_VIDEO_CODE = "i5WrsEbT4tc";
+}
